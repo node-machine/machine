29f79d9ed2a87fa8dcef3b36e381ffa89e2632e8
Mike McNeil <mike@balderdash.co>
2014-08-31 07:50:59 -0500

    Fix paths

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-// Set default require context for loading machine definitions/dependencies
-// (i.e. the module that required `node-machine`)
-
-var Machine = require('./lib/Machine');
-Machine._requireCtx = Machine._requireCtx || module.parent;
-module.exports = Machine;
+module.exports = require('./lib/Machine.constructor');
