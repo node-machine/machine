734a0f431558df3c23f3d0c02328edcd578a612a
Cody Stoltman <particlebanana@gmail.com>
2014-10-29 13:06:50 -0500

    hook up run-time input validation

---
+++ test/types/rttc.primatives.test.js
@@ -0,0 +1,80 @@
+var assert = require('assert');
+var T = require('../../lib/types');
+
+describe('Run-time type checking', function() {
+
+  describe('when primative values are used', function() {
+
+    // Build an example input schema
+    var inputSchema = {
+      foo: {
+        type: 'string',
+        required: true
+      },
+      bar: {
+        type: 'integer',
+        required: false
+      },
+      baz: {
+        type: 'boolean',
+        required: true
+      }
+    };
+
+
+    ////////////////////////////////
+    // Valid
+    ////////////////////////////////
+
+    it('should validate when all required keys are met', function() {
+      var test = {
+        foo: 'bar',
+        baz: false
+      };
+
+      assert.doesNotThrow(function() {
+        T.rttc(inputSchema, test);
+      });
+    });
+
+    it('should validate when all keys are valid', function() {
+      var test = {
+        foo: 'bar',
+        bar: 2,
+        baz: false
+      };
+
+      assert.doesNotThrow(function() {
+        T.rttc(inputSchema, test);
+      });
+    });
+
+    ////////////////////////////////
+    // Invalid
+    ////////////////////////////////
+
+    it('should not validate when all required keys are not met', function() {
+      var test = {
+        foo: 'bar'
+      };
+
+      assert.throws(function() {
+        T.rttc(inputSchema, test);
+      }, Error);
+    });
+
+    it('should not validate when all keys are not valid', function() {
+      var test = {
+        foo: 'bar',
+        bar: '2',
+        baz: false
+      };
+
+      assert.throws(function() {
+        T.rttc(inputSchema, test);
+      }, Error);
+    });
+
+  });
+
+});
