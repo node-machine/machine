37c721ff30b73795bd43328750607b114b399145
Cody Stoltman <particlebanana@gmail.com>
2014-10-29 13:06:23 -0500

    fix test

--- test/types/infer.primatives.test.js
+++ test/types/infer.primatives.test.js
@@ -6,20 +6,20 @@ describe('Inferring types from example', function() {
   describe('when primative values are used', function() {
 
     it('should set type "string"', function() {
-      var type = T.rttc.infer('foo');
+      var type = T.infer('foo');
       assert.strictEqual(type, 'string');
     });
 
     it('should set type "integer"', function() {
-      var type = T.rttc.infer(5);
+      var type = T.infer(5);
       assert.strictEqual(type, 'integer');
     });
 
     it('should set type "boolean"', function() {
-      var type = T.rttc.infer(false);
+      var type = T.infer(false);
       assert.strictEqual(type, 'boolean');
 
-      type = T.rttc.infer(true);
+      type = T.infer(true);
       assert.strictEqual(type, 'boolean');
     });
 
