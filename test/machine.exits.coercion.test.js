e9c731bea6d5d5f9d9ec92f8a71200878a74688d
Cody Stoltman <particlebanana@gmail.com>
2014-11-12 09:07:00 -0600

    basic exit coercion and type check

---
+++ test/machine.exits.coercion.test.js
@@ -0,0 +1,69 @@
+var assert = require('assert');
+var M = require('../lib/Machine.constructor');
+
+describe('Machine exit coercion', function() {
+
+  it('should return the valid data from the exit', function(done) {
+    var machine = {
+      inputs: {
+        foo: {
+          example: 'foo bar'
+        }
+      },
+      exits: {
+        success: {
+          example: 'hello'
+        },
+        error: {
+          example: 'world'
+        }
+      },
+      fn: function (inputs, exits, deps) {
+        exits(null, 'foo');
+      }
+    };
+
+    M.build(machine)
+    .configure({
+      foo: 'hello'
+    })
+    .exec(function(err, result) {
+      if(err) return done(err);
+      assert(result === 'foo');
+      done();
+    });
+  });
+
+  it('should coerce invalid exit data into the correct types', function(done) {
+    var machine = {
+      inputs: {
+        foo: {
+          example: 'foo bar'
+        }
+      },
+      exits: {
+        success: {
+          example: 4
+        },
+        error: {
+          example: 'world'
+        }
+      },
+      fn: function (inputs, exits, deps) {
+        exits(null, '100');
+      }
+    };
+
+    M.build(machine)
+    .configure({
+      foo: 'hello'
+    })
+    .exec(function(err, result) {
+      if(err) return done(err);
+      assert(result === 100);
+      done();
+    });
+  });
+
+
+});
