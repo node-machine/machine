b9c7e6d689783c8c031482726486e713f892447f
Cody Stoltman <particlebanana@gmail.com>
2014-11-12 10:16:37 -0600

    validate that machine inputs have an `example` property

---
+++ test/machine.inputs.coercion.test.js
@@ -0,0 +1,68 @@
+var assert = require('assert');
+var M = require('../lib/Machine.constructor');
+
+describe('Machine input coercion', function() {
+
+  it('should coerce input data into proper types', function(done) {
+    var _inputs;
+
+    var machine = {
+      inputs: {
+        foo: {
+          example: 100
+        },
+        bar: {
+          example: 'foobar'
+        }
+      },
+      exits: {
+        success: {
+          example: 'hello'
+        },
+        error: {
+          example: 'world'
+        }
+      },
+      fn: function (inputs, exits, deps) {
+        _inputs = inputs;
+        exits(null, 'foo');
+      }
+    };
+
+    M.build(machine)
+    .configure({
+      foo: '20',
+      bar: 20
+    })
+    .exec(function(err, result) {
+      if(err) return done(err);
+      assert(_inputs.foo === 20);
+      assert(_inputs.bar === '20')
+      done();
+    });
+  });
+
+  it('should error if an example is not given for an input', function() {
+    var machine = {
+      inputs: {
+        foo: {}
+      },
+      exits: {
+        success: {
+          example: 'hello'
+        },
+        error: {
+          example: 'world'
+        }
+      },
+      fn: function (inputs, exits, deps) {
+        exits(null, 'foo');
+      }
+    };
+
+    assert.throws(function() {
+      M.build(machine);
+    }, Error);
+  });
+
+});
