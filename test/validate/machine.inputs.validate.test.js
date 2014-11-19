9d9aa6ae554b891cc427b113bdbae331d5f001a2
Cody Stoltman <particlebanana@gmail.com>
2014-11-19 13:17:37 -0600

    add support for a validate function

---
+++ test/validate/machine.inputs.validate.test.js
@@ -0,0 +1,106 @@
+var assert = require('assert');
+var M = require('../../lib/Machine.constructor');
+
+describe('Machine inputs validate', function() {
+
+  ////////////////////////////////
+  // Valid
+  ////////////////////////////////
+
+  it('should run when validate returns true', function(done) {
+
+    var machine = {
+      inputs: {
+        foo: {
+          validate: function(inputs, env) {
+            return true;
+          }
+        }
+      },
+
+      exits: {
+        success: {},
+        error: {}
+      },
+
+      fn: function (inputs, exits, deps) {
+        exits();
+      }
+    };
+
+    M.build(machine)
+    .configure({
+      foo: {
+        bar: 'hello',
+        baz: 123
+      }
+    })
+    .exec(function(err, result) {
+      if(err) return done(err);
+      done();
+    });
+  });
+
+  ////////////////////////////////
+  // Invalid
+  ////////////////////////////////
+
+  it('should fail when validate returns false', function(done) {
+
+    var machine = {
+      inputs: {
+        foo: {
+          validate: function(inputs, env) {
+            return false;
+          }
+        }
+      },
+
+      exits: {
+        success: {},
+        error: {}
+      },
+
+      fn: function (inputs, exits, deps) {
+        exits();
+      }
+    };
+
+    M.build(machine)
+    .configure({
+      foo: {
+        bar: 'hello',
+        foo: 123
+      }
+    })
+    .exec(function(err, result) {
+      assert(err);
+      done();
+    });
+  });
+
+  it('should fail when an input is defined with a valid key', function() {
+
+    var machine = {
+      inputs: {
+        foo: {
+          valid: true
+        }
+      },
+
+      exits: {
+        success: {},
+        error: {}
+      },
+
+      fn: function (inputs, exits, deps) {
+        exits();
+      }
+    };
+
+    assert.throws(function() {
+      M.build(machine);
+    });
+  });
+
+});
