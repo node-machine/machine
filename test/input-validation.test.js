96f6c4389e5b3d6ddf6020a9a4e65f34a101351e
Mike McNeil <mike@balderdash.co>
2015-03-07 17:30:04 -0800

    Disable explicit Readable stream support temporarily due to discrepancies across node versions.

--- test/input-validation.test.js
+++ test/input-validation.test.js
@@ -189,7 +189,10 @@ describe('input validation/coercion', function (){
     { example: {}, actual: /some regexp/, result: {} },
     { example: {}, actual: function(){}, error: true },
     { example: {}, actual: new Date('November 5, 1605 GMT'), error: true },
-    { example: {}, actual: new Readable(), result: { _readableState: { highWaterMark: 16384, buffer: [], length: 0, pipes: null, pipesCount: 0, flowing: false, ended: false, endEmitted: false, reading: false, calledRead: false, sync: true, needReadable: false, emittedReadable: false, readableListening: false, objectMode: false, defaultEncoding: 'utf8', ranOut: false, awaitDrain: 0, readingMore: false, decoder: null, encoding: null }, readable: true, domain: null, _events: {}, _maxListeners: 10 } },
+
+    // Skip Readable stream tests for now since the enumerable properties vary between Node.js versions.
+    // TODO: bring back support for this by explicitly filtering properties of streams in `.exec()`
+    // { example: {}, actual: new Readable(), result: { _readableState: { highWaterMark: 16384, buffer: [], length: 0, pipes: null, pipesCount: 0, flowing: false, ended: false, endEmitted: false, reading: false, calledRead: false, sync: true, needReadable: false, emittedReadable: false, readableListening: false, objectMode: false, defaultEncoding: 'utf8', ranOut: false, awaitDrain: 0, readingMore: false, decoder: null, encoding: null }, readable: true, domain: null, _events: {}, _maxListeners: 10 } },
     { example: {}, actual: new Buffer('asdf'), error: true },
     { example: {}, actual: new Error('asdf'), result: {} },  // TODO: consider enhancing this behavior to guarantee e.g. `.message` (string), `.stack` (string), `.code` (string), and `.status` (number).  Needs community discussion
 
