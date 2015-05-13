b3babcb9959fe7c493ed74b77e34f250762e5a26
Mike McNeil <mike@balderdash.co>
2015-05-12 19:11:30 -0500

    Fix exit coercion test helper-- completes the refactor.  All 3906 tests pass.

--- test/helpers/test-exit-coercion.helper.js
+++ test/helpers/test-exit-coercion.helper.js
@@ -8,7 +8,7 @@ var Machine = require('../../');
 
 
 
-module.exports = function testExitCoercion(options, cb){
+module.exports = function testExitCoercion(expectations, cb){
   var _inputsInFn;
   var machine = Machine.build({
     inputs: {},
@@ -16,29 +16,53 @@ module.exports = function testExitCoercion(options, cb){
       error: {},
       success: (function _determineExitDefinition(){
         var def = {};
-        if (!_.isUndefined(options.example)) {
-          def.example = options.example;
+        if (!_.isUndefined(expectations.example)) {
+          def.example = expectations.example;
         }
-        if (!_.isUndefined(options.getExample)) {
-          def.getExample = options.getExample;
+        if (!_.isUndefined(expectations.getExample)) {
+          def.getExample = expectations.getExample;
         }
-        if (!_.isUndefined(options.void)) {
-          def.void = options.void;
+        if (!_.isUndefined(expectations.void)) {
+          def.void = expectations.void;
         }
         return def;
       })()
     },
     fn: function (inputs, exits) {
-      exits(null, options.actual);
+      exits(null, expectations.actual);
     }
   })
-  .exec(function (err, result){
+  .exec(function (err, actualResult){
     if (err) return cb(new Error('Unexpected error:'+require('util').inspect(err, false, null)));
 
-    // validate against expected output
-    if (!_.isEqual(result, options.result)){
-      return cb(new Error('Unexpected result; got:' + util.inspect(result,false, null)));
+    // If an expected `result` is provided, compare the actual result against that.
+    // Otherwise compare it against the original value (`actual`)
+    var compareTo = expectations.hasOwnProperty('result') ? expectations.result : expectations.actual;
+
+    // if `result` is set, use a lodash equality check
+    if (!_.isEqual(actualResult, compareTo)) {
+      return cb(new Error('returned incorrect value: '+util.inspect(actualResult, false, null)));
     }
+
+    // Test using strict equality (===) if explicitly requested
+    if (expectations.strictEq) {
+      if (actualResult !== compareTo) {
+        return cb(new Error('returned value is equivalent (but not ===)'));
+      }
+    }
+
+    // Test AGAINST strict equality using `isNew` if requested
+    // (i.e. guarantees this is a new value and is !== what was passed in)
+    if (expectations.isNew) {
+
+      // Check both the expected result and the actual value, just to be safe.
+      // (should never even be possible for it to be a direct reference to the expected result)
+      if (actualResult === compareTo || actualResult === expectations.actual) {
+        return cb(new Error('returned value === value that was passed in -- but should have been a new value!'));
+      }
+    }
+
+    // If we made it here, everything's good!
     return cb();
   });
 };
