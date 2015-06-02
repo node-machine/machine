4fd19b0ab15c7af17bc62c0e14cd8ab56645ee21
Mike McNeil <mike@balderdash.co>
2015-06-02 10:29:25 -0500

    A deeper explanation of the 'typesafe' flag.

--- spec/machine-def.struct.js
+++ spec/machine-def.struct.js
@@ -18,6 +18,26 @@ module.exports = {
   idempotent: true,
 
 
+  // The `typesafe` flag is disabled by default.  If a machine is `typesafe`,
+  // that means it will run in "unsafe mode" (unless overridden in userland).
+  //
+  // That means:
+  //  1. Its configured input values will not be cloned before exec()ing the fn.
+  //  2. Its outputs will not be cloned before being passed back to the user.
+  //
+  // So if a machine sets this flag, it is guaranteeing that:
+  //  1. The machine's `fn` will never throw in a callback, EVEN if a completely wrong
+  //     input value is provided, or a required value is omitted. (TODO: reconsider--
+  //     this is a pretty restrictive requirement. Could do some really cheap,
+  //     surface-level validation of inputs when `typesafe` is enabled.  That would
+  //     loosen this up a bit without any significant performance cost)
+  //  2. The machine's `fn` will output values exactly according to the type schema
+  //     inferred from its exit example.
+  //  3. The machine does not mutate any of the provided input values inside its `fn`.
+  //  4. The user has exclusive access to its outputs-- that is, they may be mutated
+  //     by the user without causing any terrible side-effects.  That means they are
+  //     either primitive values, or objects that were constructed within the `fn`
+  //     itself, and are used nowhere else.
   typesafe: true,
 
 
