50c6c00270f2600a7a53cded507de719ce791ae1
Mike McNeil <mike@balderdash.co>
2015-06-20 21:39:29 -0500

    Added an additional timeout test to verify that machine exit callbacks are not triggered more than once.

--- test/timeout.test.js
+++ test/timeout.test.js
@@ -9,12 +9,11 @@ var Machine = require('../');
 describe('timeout', function (){
 
   describe('machine that never triggers its exits', function (){
-
-    describe('with timeout configured for 1 second', function (){
+    describe('with timeout configured for 0.25 seconds', function (){
       var machineInstance;
       before(function (){
         machineInstance = Machine.build({
-          timeout: 1000,
+          timeout: 250,
           fn: function machineThatNeverTriggersItsExits(inputs, exits, env){
             // beep
           }
@@ -34,7 +33,37 @@ describe('timeout', function (){
           }
         });
       });
+    });
+  });
 
+
+  describe('machine that triggers its exits after 0.5 seconds', function (){
+    describe('with timeout configured for 0.25 seconds', function (){
+      var machineInstance;
+      before(function (){
+        machineInstance = Machine.build({
+          timeout: 250,
+          fn: function machineThatNeverTriggersItsExits(inputs, exits, env){
+            setTimeout(function (){
+              return exits.success();
+            }, 500);
+          }
+        });
+      });
+
+      it('should call its error exit with a timeout error', function (done){
+        machineInstance().exec({
+          error: function (err) {
+            if (err.code === 'E_TIMEOUT') {
+              return done();
+            }
+            return done(new Error('Expecting Error instance w/ `code` === "E_TIMEOUT", but instead got this error (code:'+err.code+'): '+err.stack));
+          },
+          success: function (){
+            return done(new Error('Should not have called the success exit (should have been prevented by timeout alarm)'));
+          }
+        });
+      });
     });
   });
 
