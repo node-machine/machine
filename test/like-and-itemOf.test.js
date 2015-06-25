fe22dfb79ac56e85b2f25feb7120cb81eb6ccef1
Mike McNeil <mike@balderdash.co>
2015-06-24 18:16:03 -0700

    Added tests for 'like' and 'itemof'

---
+++ test/like-and-itemOf.test.js
@@ -0,0 +1,346 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+var Machine = require('../');
+var testMachineWithMocha = require('test-machinepack-mocha').testMachineWithMocha;
+
+
+
+describe('`like` and `itemOf`', function (){
+
+  describe('using `like` in one of its exits', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: { fullName: { example: 'Roger Rabbit' } },
+      exits: { success: { like: 'fullName' } },
+      fn: function (inputs, exits) { return exits.success(123); }
+    }))
+    .use({})
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `like` in one of its inputs', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: 'Roger Rabbit' },
+        firstName: { like: 'fullName' }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) { return exits.success(inputs.firstName); }
+    }))
+    .use({ firstName: 123 })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `itemOf` in one of its exits', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: ['Roger'] }
+      },
+      exits: { success: { itemOf: 'fullName' } },
+      fn: function (inputs, exits) { return exits.success(123); }
+    }))
+    .use({})
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `itemOf` in one of its inputs', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: ['Roger'] },
+        firstName: { itemOf: 'fullName' }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) { return exits.success(inputs.firstName); }
+    }))
+    .use({ firstName: 123 })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `like` in one of its contract\'s exits', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: 'Roger' },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            exits: { success: { like: 'fullName' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) { return exits.success( inputs.getFullName().execSync() ); }
+    }))
+    .use({
+      getFullName: function (unused, exits){
+        exits.success(123);
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `like` in one of its contract\'s inputs', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: 'Roger' },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            inputs: { name: { like: 'fullName' } },
+            exits: { success: { example: '===' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) { return exits.success( inputs.getFullName({ name: 123 }).execSync() ); }
+    }))
+    .use({
+      getFullName: function (inputs, exits){
+        return exits.success(inputs.name);
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `itemOf` in one of its contract\'s exits', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: ['Roger'] },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            exits: { success: { itemOf: 'fullName' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) { return exits.success( inputs.getFullName().execSync() ); }
+    }))
+    .use({
+      getFullName: function (unused, exits){
+        exits.success(123);
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `itemOf` in one of its contract\'s inputs', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: ['Roger'] },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            inputs: { name: { itemOf: 'fullName' } },
+            exits: { success: { example: '===' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) { return exits.success( inputs.getFullName({ name: 123 }).execSync() ); }
+    }))
+    .use({
+      getFullName: function (inputs, exits){
+        return exits.success(inputs.name);
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `like` in one of its contract\'s inputs\' contract\'s exits', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: 'Roger' },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            inputs: {
+              toCompleteName: {
+                example: '->',
+                contract: {
+                  sync: true,
+                  exits: { success: { like: 'fullName' } }
+                }
+              }
+            },
+            exits: { success: { example: '===' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) {
+        return exits.success( inputs.getFullName({
+          toCompleteName: function (unused, exits){
+            return exits.success(123);
+          }
+        }).execSync() );
+      }
+    }))
+    .use({
+      getFullName: function (inputs, exits){
+        return exits.success( inputs.toCompleteName().execSync() );
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `like` in one of its contract\'s inputs\' contract\'s inputs', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: 'Roger' },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            inputs: {
+              toCompleteName: {
+                example: '->',
+                contract: {
+                  sync: true,
+                  inputs: { name: { like: 'fullName' } },
+                  exits: { success: { example: '===' } }
+                }
+              }
+            },
+            exits: { success: { example: '===' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) {
+        return exits.success( inputs.getFullName({
+          toCompleteName: function (inputs, exits){
+            return exits.success(inputs.name);
+          }
+        }).execSync() );
+      }
+    }))
+    .use({
+      getFullName: function (inputs, exits){
+        return exits.success( inputs.toCompleteName({name:123}).execSync() );
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `itemOf` in one of its contract\'s inputs\' contract\'s exits', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: ['Roger'] },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            inputs: {
+              toCompleteName: {
+                example: '->',
+                contract: {
+                  sync: true,
+                  exits: { success: { itemOf: 'fullName' } }
+                }
+              }
+            },
+            exits: { success: { example: '===' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) {
+        return exits.success( inputs.getFullName({
+          toCompleteName: function (unused, exits){
+            return exits.success(123);
+          }
+        }).execSync() );
+      }
+    }))
+    .use({
+      getFullName: function (inputs, exits){
+        return exits.success( inputs.toCompleteName().execSync() );
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+  describe('using `itemOf` in one of its contract\'s inputs\' contract\'s inputs', function (){
+    testMachineWithMocha().machine(Machine.build({
+      inputs: {
+        fullName: { example: ['Roger'] },
+        getFullName: {
+          example: '->',
+          contract: {
+            sync: true,
+            inputs: {
+              toCompleteName: {
+                example: '->',
+                contract: {
+                  sync: true,
+                  inputs: { name: { itemOf: 'fullName' } },
+                  exits: { success: { example: '===' } }
+                }
+              }
+            },
+            exits: { success: { example: '===' } }
+          }
+        }
+      },
+      exits: { success: { example: '===' } },
+      fn: function (inputs, exits) {
+        return exits.success( inputs.getFullName({
+          toCompleteName: function (inputs, exits){
+            return exits.success(inputs.name);
+          }
+        }).execSync() );
+      }
+    }))
+    .use({
+      getFullName: function (inputs, exits){
+        return exits.success( inputs.toCompleteName({ name: 123 }).execSync() );
+      }
+    })
+    .expect({
+      outcome: 'success',
+      output: '123'
+    });
+  });
+
+
+});
