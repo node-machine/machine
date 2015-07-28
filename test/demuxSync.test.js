bd36cb00c10f5ac2cc03b2d47feec0d7db76acff
Mike McNeil <mike@balderdash.co>
2015-07-28 10:30:03 -0500

    Expand demuxSync() with an optional argument that allows the 'desired' exit to be customized (i.e. be something other than 'success').  Also, this validates that new optional argument, if it is specified (including making sure it is a name of a known exit).  Finally, this commit also includes tests. Remember: demuxSync() is experimental-- use at your own risk

--- test/demuxSync.test.js
+++ test/demuxSync.test.js
@@ -4,64 +4,91 @@ var Machine = require('../');
 
 describe('Machine.prototype.demuxSync()', function (){
 
-  it('should throw when called on a machine that does not declare sync:true', function (){
-    try {
-      Machine.build(machineFixtures[0]).demuxSync();
-    }
-    catch (e) {
-      assert.equal(e.code,'E_USAGE');
-      return;
-    }
+  describe('with no arguments', function (){
+    it('should throw when called on a machine that does not declare sync:true', function (){
+      try {
+        Machine.build(machineFixtures[0]).demuxSync();
+      }
+      catch (e) {
+        assert.equal(e.code,'E_USAGE');
+        return;
+      }
 
-    // Should never make it here
-    assert(false, 'Should have thrown an error');
+      // Should never make it here
+      assert(false, 'Should have thrown an error');
+    });
+
+
+    it('should return `true` when called on a configured machine that triggers its success exit', function (){
+      var result = Machine.build(machineFixtures[1]).demuxSync();
+      assert.equal(result,true);
+    });
+
+    it('should return `false` when called on a configured machine that triggers any non-success exit and sends back an Error instance', function (){
+      var m = Machine.build(machineFixtures[2]);
+      m.configure({value: 'hi'});
+      assert.equal(m.demuxSync(), false);
+    });
+
+    it('should return `false` when called on a configured machine that triggers any non-success exit and sends back a string', function (){
+      var m = Machine.build(machineFixtures[2]);
+      m.configure({value: 'hi'});
+      assert.equal(m.demuxSync(), false);
+    });
+
+    it('should return `false` when called on a configured machine that triggers any non-success exit and sends back a boolean', function (){
+      var m = Machine.build(machineFixtures[2]);
+      m.configure({value: true});
+      assert.equal(m.demuxSync(), false);
+    });
+
+    it('should return `false` when called on a configured machine that triggers any non-success exit and sends back a number', function (){
+      var m = Machine.build(machineFixtures[2]);
+      m.configure({value: -234.2});
+      assert.equal(m.demuxSync(), false);
+    });
+
+    it('should return `false` when called on a configured machine that triggers any non-success exit and sends back an array', function (){
+      var m = Machine.build(machineFixtures[2]);
+      m.configure({value: ['stuff', 'and', 'things']});
+      assert.equal(m.demuxSync(), false);
+    });
+
+    it('should return `false` when called on a configured machine that triggers any non-success exit and sends back an object', function (){
+      var m = Machine.build(machineFixtures[2]);
+      m.configure({value: {
+        email: 'ricketorsomething@stark.io'
+      }});
+      assert.equal(m.demuxSync(), false);
+    });
   });
 
 
-  it('should return `true` when called on a configured machine that triggers its success exit', function (){
-    var result = Machine.build(machineFixtures[1]).demuxSync();
-    assert.equal(result,true);
+  describe('with custom exit name specified', function (){
+    it('should return `true` when called on a configured machine that triggers the specified exit', function (){
+      var result = Machine.build(machineFixtures[2]).demuxSync('someOtherExit');
+      assert.equal(result,true);
+    });
+
+    it('should return `false` when called on a configured machine that triggers any other exit', function (){
+      var result = Machine.build(machineFixtures[1]).demuxSync('someOtherExit');
+      assert.equal(result,false);
+    });
   });
 
-  it('should return `false` when called on a configured machine that triggers any non-success exit and sends back an Error instance', function (){
-    var m = Machine.build(machineFixtures[2]);
-    m.configure({value: 'hi'});
-    assert.equal(m.demuxSync(), false);
-  });
-
-  it('should return `false` when called on a configured machine that triggers any non-success exit and sends back a string', function (){
-    var m = Machine.build(machineFixtures[2]);
-    m.configure({value: 'hi'});
-    assert.equal(m.demuxSync(), false);
-  });
-
-  it('should return `false` when called on a configured machine that triggers any non-success exit and sends back a boolean', function (){
-    var m = Machine.build(machineFixtures[2]);
-    m.configure({value: true});
-    assert.equal(m.demuxSync(), false);
-  });
-
-  it('should return `false` when called on a configured machine that triggers any non-success exit and sends back a number', function (){
-    var m = Machine.build(machineFixtures[2]);
-    m.configure({value: -234.2});
-    assert.equal(m.demuxSync(), false);
-  });
-
-  it('should return `false` when called on a configured machine that triggers any non-success exit and sends back an array', function (){
-    var m = Machine.build(machineFixtures[2]);
-    m.configure({value: ['stuff', 'and', 'things']});
-    assert.equal(m.demuxSync(), false);
-  });
-
-  it('should return `false` when called on a configured machine that triggers any non-success exit and sends back an object', function (){
-    var m = Machine.build(machineFixtures[2]);
-    m.configure({value: {
-      email: 'ricketorsomething@stark.io'
-    }});
-    assert.equal(m.demuxSync(), false);
-  });
 
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -104,6 +131,7 @@ var machineFixtures = [
       success: {
         example: 'hello'
       },
+      someOtherExit: {},
       error: {
         example: 'world'
       }
