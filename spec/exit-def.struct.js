1165a65ec994625f49623bc785a0b51b7b98bc81
Mike McNeil <mike@balderdash.co>
2016-07-16 15:02:59 -0500

    Add notes about deprecating the use of like/itemOf in top-level machine input definitions.  Also expand documentation for machine exit defs.

--- spec/exit-def.struct.js
+++ spec/exit-def.struct.js
@@ -1,5 +1,9 @@
 module.exports = {
-  friendlyName: 'some exit',
-  description: 'Returns a list of gummy worm flavors.',
-  example: '==='
+  friendlyName: 'Unrecognized flavors',
+  description: 'Could not recognize one or more of the provided `flavorStrings`.', // >> A short explanation of what it means if the machine's `fn` calls this exit at runtime.  Written as a post-mortem analysis (past tense).
+  extendedDescription: 'Some **markdown**.', // >> Like any other `extendedDescription`, supports markdown.
+  moreInfoUrl: 'http://gummyworms.com/flavors',
+  outputFriendlyName: 'Unrecognized flavors', // >> e.g. if var name would normally be `unrecognizedFlavors`
+  outputDescription: 'A list of invalid gummy worm flavors.', // >> noun phrase
+  example: '===' // >> an example schema describing the output (technically, an RTTC exemplar schema).  Omit this property to indicate no output.
 };
