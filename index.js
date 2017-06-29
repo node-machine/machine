1e7c62fda452f745af60a2dc7c0f42c89a5db718
Mike McNeil <michael.r.mcneil@gmail.com>
2017-06-29 17:05:53 +0200

    One last test case.

--- index.js
+++ index.js
@@ -562,6 +562,9 @@ module.exports = function buildCallableMachine(nmDef){
       // ```
       // var inner = require('./')({ timeout: 55, exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } }); require('./')({ identity: 'outer', timeout: 50, exits: { foo: { description: 'Not the same' }}, fn: function(inputs, exits) { inner({}).exec((err)=>{ if (err) { return exits.error(err); } return exits.success(); }); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
       // ```
+      // ```
+      // var inner = require('./')({ timeout: 45, exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } }); require('./')({ identity: 'outer', timeout: 50, exits: { foo: { description: 'Not the same' }}, fn: function(inputs, exits) { inner({}).exec((err)=>{ if (err) { return exits.error(err); } return exits.success(); }); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
+      // ```
       // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
       // Pass in the omen, if we were able to create one.
