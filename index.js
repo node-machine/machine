35520dd0002dcd0e5b71184fb79ef1d8977b301e
Mike McNeil <michael.r.mcneil@gmail.com>
2017-07-03 20:24:00 +0200

    Remove examples (see c4e738e8016771bd55b78cba277e62a83d5c61fc for future reference)

--- index.js
+++ index.js
@@ -611,30 +611,10 @@ module.exports = function buildCallableMachine(nmDef){
       //                                                            
       // If provided, use the timeout (max # of ms to wait for this machine to finish executing)
       nmDef.timeout || undefined,
-      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-      // FUTURE: Write tests that ensure all of the following scenarios are working:
-      // ```
-      // require('./')({ timeout: 50, exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
-      // ```
-      // ```
-      // var inner = require('./')({ timeout: 50, exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } }); require('./')({ identity: 'outer', exits: { foo: { description: 'Not the same' }}, fn: function(inputs, exits) { inner({}).exec((err)=>{ if (err) { return exits.error(err); } return exits.success(); }); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
-      // ```
-      // ```
-      // var inner = require('./')({ exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } }); require('./')({ identity: 'outer', timeout: 50, exits: { foo: { description: 'Not the same' }}, fn: function(inputs, exits) { inner({}).exec((err)=>{ if (err) { return exits.error(err); } return exits.success(); }); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
-      // ```
-      // ```
-      // var inner = require('./')({ timeout: 55, exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } }); require('./')({ identity: 'outer', timeout: 50, exits: { foo: { description: 'Not the same' }}, fn: function(inputs, exits) { inner({}).exec((err)=>{ if (err) { return exits.error(err); } return exits.success(); }); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
-      // ```
-      // ```
-      // var inner = require('./')({ timeout: 45, exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } }); require('./')({ identity: 'outer', timeout: 50, exits: { foo: { description: 'Not the same' }}, fn: function(inputs, exits) { inner({}).exec((err)=>{ if (err) { return exits.error(err); } return exits.success(); }); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
-      // ```
-      //
-      //
-      // NOTE THAT THIS WILL NEVER WORK IN THE SAME WAY (because it's using the bare, explicit cb usage):
-      // ```
-      // var inner = require('./')({ timeout: 50, exits: { foo: {description: 'Whoops' } }, fn: function(inputs, exits) { setTimeout(()=>{return exits.foo(987);},750); } }); require('./')({ identity: 'outer', exits: { foo: { description: 'Not the same' }}, fn: function(inputs, exits) { inner({}, (err)=>{ if (err) { return exits.error(err); } return exits.success(); }); } })().switch({ error: (err)=>{ console.log('Got error:',err); }, foo: ()=>{ console.log('Should NEVER make it here.  The `foo` exit of some other machine in the implementation has nothing to do with THIS `foo` exit!!'); }, success: ()=>{ console.log('Got success.'); }, });
-      // ```
-      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      // FUTURE: Write tests that ensure all of the scenarios in c4e738e8016771bd55b78cba277e62a83d5c61fc are working.
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      
 
       //   ██████╗ ███╗   ███╗███████╗███╗   ██╗
       //  ██╔═══██╗████╗ ████║██╔════╝████╗  ██║
